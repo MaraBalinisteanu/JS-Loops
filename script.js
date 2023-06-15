fe708ee7c9d9567c87f75bb3073f3429b839e968
Mara_Balinisteanu <balinisteanumara@yahoo.com>
2023-06-15 17:07:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+//calculati media aritmetica a unui array de numere;
+//avand un array cu numere, valori boolene si stringuri calculati suma tuturor numerelor. Ignorati toate valorile care nu sunt de tip numar;
+//BONUS: creati un program care afiseaza primele 20 numere din sirul lui Fibonacci;
